c3e1c1744bbd3779816967b8c2161a0b1e3131e3
Edna <ednnas51@gmail.com>
2026-04-10 02:09:35 -0300

    fix sitemap build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://blog-afiliado.vercel.app/',
+  site: 'https://blog-afiliado.vercel.app',
   integrations: [sitemap()],
+  output: 'static', // 👈 ESSENCIAL
   vite: {
     plugins: [tailwindcss()],
   },
