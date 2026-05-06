71d699c15e840c461d5fb6a2625da31f523de5b9
Edna <ednnas51@gmail.com>
2026-05-05 22:16:08 -0300

    ajustes de código e link

--- src/data/products.ts
+++ src/data/products.ts
@@ -17,7 +17,7 @@ export const produtos = {
 		ml: 'https://www.mercadolivre.com.br/fogo-de-piso-5-bocas-consul-cfs5nar-acendimento-automatico-cor-inox/p/MLB15295566?matt_tool=38524122',
 		shopee: 'https://s.shopee.com.br/1gExh55W8A?lp=aff',
 	},
-	electrolux76exv: {
-		ml: 'https://meli.la/1yy3jCm',
+	electroluxFE5IG: {
+		ml: 'https://www.mercadolivre.com.br/fogo-5-bocas-electrolux-cinza-mesa-inox-perfectcook-fe5ig/p/MLB58200579?pdp_filters=item_id%3AMLB4510248811&matt_event_ts=1778028554553&matt_d2id=f3daa5c6-aabd-43a1-9dec-ee1d4967e855&matt_tracing_id=49d6633b-aa48-4908-96f2-9aada3f8c220',
 	},
 } as const satisfies Record<string, ProdutoAfiliado>;
